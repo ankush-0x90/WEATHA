4de72ac6071321815c37bf99bc68a1957fcd6a1a
Ankush Patil <aspraz2658@gmail.com>
2018-04-02 12:32:42 +0530

    Modified Ui

--- WEATHA/app/navigation/Router.js
+++ WEATHA/app/navigation/Router.js
@@ -2,32 +2,26 @@
 
 import React, {Component} from 'react';
 import {AppRegistry, ScrollView, Text, View} from 'react-native';
+
 import {
   DrawerItems,
   DrawerNavigator,
   StackNavigator,
   TabNavigator
 } from 'react-navigation';
+
+
 import {HomeScreen} from '../HomeScreen';
 import {DetailsScreen1} from '../DetailsScreen1';
 import {DetailsScreen2} from '../DetailsScreen2';
 import {SettingsScreen} from '../SettingsScreen';
+
 import * as css from '../Styles';
 import {Icon} from 'react-native-elements';
+
 import {Provider} from 'react-redux';
 import {store} from '../state/Context';
 
-/**
- * This is where the navigation hierarchy for the app is setup using DrawerNavigator,
- * StackNavigator, and TabNavigator.
- *
- * The Redux store is also wired into the Provider, which sits at the root of the view
- * hierarchy.
- */
-
-//
-// TABS
-//
 
 const NavTab = TabNavigator(
   // route config
@@ -51,7 +45,7 @@ const NavTab = TabNavigator(
 const titleAndIcon =
         <View style={css.header.container}>
           <Icon name="wb-sunny" color={css.colors.text_light}/>
-          <Text style={css.header.text}>Weather App</Text>
+          <Text style={css.header.text}>WEATHA</Text>
         </View>;
 
 const NavStack = StackNavigator(
@@ -72,14 +66,8 @@ const NavStack = StackNavigator(
   },
 );
 
-//
-// DRAWER
-//
-
-/**
- * drawer ... more info https://goo.gl/2Dnmtl
- */
 const customDrawerComponent = (props) =>
+  
   <ScrollView
     style={{
       flex           : 1,
@@ -88,24 +76,20 @@ const customDrawerComponent = (props) =>
     <DrawerItems {...props} />
   </ScrollView>;
 
-/**
- * more info on why this variable name must begin with CAPITAL letters due to JSX syntax
- * https://goo.gl/nGRaAl
- */
 const NavDrawer = DrawerNavigator(
   // route config
   {
     HomeRoute    : {
       screen           : NavStack,
       navigationOptions: {
-        drawerLabel: 'Main App',
+        drawerLabel: 'Weatha',
         drawerIcon : ({tintColor}) => <Icon name="wb-sunny" color={tintColor}/>,
       },
     },
     SettingsRoute: {
       screen           : SettingsScreen,
       navigationOptions: {
-        drawerLabel: 'Settings',
+        drawerLabel: 'About Team',
         drawerIcon : ({tintColor}) => <Icon name="settings" color={tintColor}/>,
       },
     },
@@ -119,12 +103,6 @@ const NavDrawer = DrawerNavigator(
   },
 );
 
-/**
- * Place the Provider at the root of the view hierarchy for Redux.
- *
- * Note that JSX syntax requires the name to start with an uppercase letter!
- * more info - https://goo.gl/nGRaAl
- */
 class Root extends Component {
   render() {
     return (
@@ -135,5 +113,4 @@ class Root extends Component {
   }
 }
 
-// wire the WeatherApp to the AppRegistry
 AppRegistry.registerComponent('WeatherApp', () => Root);
