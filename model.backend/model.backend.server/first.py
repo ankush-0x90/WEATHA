424ee5565237f5772dd3b3e1a804fb427c0b595e
Ankush Patil <aspraz2658@gmail.com>
2018-04-05 08:51:45 +0530

    learned concepts of boto3

--- model.backend/model.backend.server/first.py
+++ model.backend/model.backend.server/first.py
@@ -3,8 +3,20 @@ from flask import jsonify
 
 app = Flask(__name__)
 
+d = {"hello": "worldd"}
+
+
 @app.route('/')
 def hello_word():
-    d = {"hello": "world"}
     return jsonify(d)
-    
+
+
+
+@app.route('/<userName>')
+def print_userName(userName):
+    d["userName"]= userName
+    return jsonify(d)
+
+
+if __name__=='__main__':
+    app.run(debug=True)
